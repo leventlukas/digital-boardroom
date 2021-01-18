80dd28b180b3d3f5955a87d955a4f8c31e6e0eee
leventlukas <levent.lukas@gmail.com>
2021-01-18 19:58:07 +0100

    added cleanup sql file

---
+++ cleanup_db.sql
@@ -0,0 +1,7 @@
+DELETE FROM Auslastung_Maschine;
+DELETE FROM StatusVerlauf_Maschine;
+DELETE FROM ProduktivitaetVerlauf_Maschine;
+DELETE FROM Komponente;
+DELETE FROM Lager_Auto;
+DELETE FROM Bestellung;
+DELETE FROM Auto;
